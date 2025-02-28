d0dd5577096cfea9435ced56dcb7a288d6e15a06
songoku776 <craftandart7@gamil.com>
2025-02-28 22:19:42 +0600

    py c3 act1

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-a = 10
-print("type of a:",type(a))
-b = 10.10
-print("type of a:",type(b))
-c = True
-print("type of a:",type(c))
-d = "HI"
-print("type of a:",type(d))
+num1 = 23
+num2 = 43
+num3 = 86
+num4 = 46
+num5 = 75
+num6 = 24
+
+sum  = num1 + num2 + num3 + num4 +num5 +num6
+print("total of this matrix is" , sum)
+
+avg = sum/6
+print("AVG of this matrix is" , avg)
